e9a14f5ffb51048fb78cb7cfd49df780597a2f76
piotrokrutniak <91792866+piotrokrutniak@users.noreply.github.com>
2023-06-30 00:50:44 +0200

    Images domain setup

---
+++ Source/Microservices/ImagesHost/Core/Domain/Models/Bases/DiscountBase.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models.Bases
+{
+    public abstract class DiscountBase : AuditableBaseEntity
+    {
+        public int DiscountAmount { get; set; }
+        public DateTime Activation { get; set; }
+        public DateTime Expiry { get; set; }
+    }
+}

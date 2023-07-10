55dbe9a78e90308b85bc14214ed667d2a6329934
piotrokrutniak <91792866+piotrokrutniak@users.noreply.github.com>
2023-07-10 19:21:55 +0200

    Fixing mismatched nuget versions, making updatedBy property optional

--- Source/Microservices/Basket/Core/Domain/Models/Common/AuditableBaseEntity.cs
+++ Source/Microservices/Basket/Core/Domain/Models/Common/AuditableBaseEntity.cs
@@ -9,7 +9,7 @@ namespace Domain.Models.Common
         public virtual int Id { get; set; }
         public string CreatedBy { get; set; }
         public DateTime Created { get; set; }
-        public string LastModifiedBy { get; set; }
+        public string? LastModifiedBy { get; set; }
         public DateTime? LastModified { get; set; }
     }
 }
